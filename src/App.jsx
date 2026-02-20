f6eba605840dd1c5004b55c8004ec4673d860770
niki-70 <nikitachouhan70143@gmail.com>
2026-02-20 11:48:15 +0530

    testimonial card and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import TeamCard from "./components/TeamCard/TeamCard"
 import ProductCard from "./components/ProductCard/ProductCard"
+import TestimonialCard from "./components/TestimonialCard/TestimonialCard"
+import Navbar from "./components/layout/Navbar/Navbar"
 
 function App() {
 
   return (
     <>
-      <TeamCard/>
+      <Navbar/>
+      {/* <TeamCard/>
       <ProductCard/>
+      <TestimonialCard/> */}
     </>
   )
 }
