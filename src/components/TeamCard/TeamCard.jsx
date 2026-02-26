f8df4e7fa0684b34641bc3e6e08951e304247bd1
niki-70 <nikitachouhan70143@gmail.com>
2026-02-26 14:31:08 +0530

    converted module css to tailwind of product card team card & testimonial card

--- src/components/TeamCard/TeamCard.jsx
+++ src/components/TeamCard/TeamCard.jsx
@@ -1,40 +1,37 @@
-import styles from "./TeamCard.module.css";
-
-const TeamCard = () => {
-  console.log(styles);
+function TeamCard() {
   return (
     <>
-      <div className={styles.teamCard}>
-        <div className={styles.teamCardImg}>
-          <div className={styles.teamCardFade}>
-            <div className={styles.teamCardIcons}>
-              <div className={styles.teamCardIcon}>
+      <div className=" group teamCard h-2xl mt-7 bg-white w-max rounded border border-white">
+        <div className="teamCardImg w-80 h-72 relative object-cover">
+          <div className="teamCardFade opacity-0 group-hover:opacity-100 rounded-t-lg w-full h-full absolute bg-black/30 grid place-items-center">
+            <div className="teamCardIcons flex">
+              <div className="teamCardIcon m-1 w-10 h-10 grid place-items-center rounded-6xl cursor-pointer hover: ">
                 <img src="src/assets/Images/icons/Vector.png" alt="" />
               </div>
-              <div className={styles.teamCardIcon}>
+              <div className="teamCardIcon m-1 w-10 h-10 grid place-items-center rounded-6xl cursor-pointer">
                 <img src="src/assets/Images/icons/Vector (1).png" alt="" />
               </div>
-              <div className={styles.teamCardIcon}>
+              <div className="teamCardIcon m-1 w-10 h-10 grid place-items-center rounded-6xl cursor-pointer">
                 <img src="src/assets/Images/icons/Vector (2).png" alt="" />
               </div>
-              <div className={styles.teamCardIcon}>
+              <div className="teamCardIcon m-1 w-10 h-10 grid place-items-center rounded-6xl cursor-pointer">
                 <img src="src/assets/Images/icons/Vector (3).png" alt="" />
               </div>
             </div>
           </div>
-          <img
-            className={styles.teamCardBg}
+           <img
+            className="teamCardBg rounded-t-lg w-auto"
             src="src/assets/Images/Teams/a80883dc1ed057e28b4f1bee4e87ed572c7cf693.jpg"
             alt=""
           />
         </div>
-        <div className={styles.teamCardInfo}>
-          <h2>Jenny Wilson</h2>
-          <p>Ceo & Founder</p>
+        <div className="teamCardInfo mt-5 mb-5 ml-5">
+          <h2 className="mt-1 mr-2.5 mb-1 ml-2.5 ">Jenny Wilson</h2>
+          <p className="text-gray-500 mt-1 mr-2.5 mb-1 ml-2.5">Ceo & Founder</p>
         </div>
       </div>
     </>
   );
-};
+}
 
 export default TeamCard;
--- src/components/layout/Navbar/Navbar.jsx
+++ src/components/layout/Navbar/Navbar.jsx
@@ -1,31 +1,14 @@
-import Styles from "./Navbar.module.css";
-import SearchBar from "./SearchBar";
-import Topbar from "./Topbar";
+import Navigation from "./Sections/Navigation";
+import SearchBar from "./Sections/SearchBar";
+import Topbar from "./Sections/Topbar";
 
-const Navbar = () => {
-  console.log(Styles);
+function Navbar() {
   return (
     <>
       <Topbar />
       <hr />
       <SearchBar />
-
-      <div className={Styles.navigation}>
-          <div class={Styles.navbar}>
-            <div className={Styles.list}>
-              <a href="#">Home</a>
-              <a href="#">Shop</a>
-              <a href="#">Blog</a>
-              <a href="#">About Us</a>
-              <a href="#">Contact Us</a>
-            </div>
-
-            <div class={Styles.contactNo}>
-              <img src="src/assets/Images/icons/call.png" alt="" />
-              <h5>(219) 555-0114</h5>
-            </div>
-          </div>
-      </div>
+      <Navigation/>
     </>
   );
 };
